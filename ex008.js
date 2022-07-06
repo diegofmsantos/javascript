de397e7f5f2d75e03b2fa5def358aa270bf2a3b4
Diego Santos <105954958+diegofmsantos@users.noreply.github.com>
2022-07-06 20:30:09 -0300

    att

---
+++ ex008.js
@@ -0,0 +1,10 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou acima do limite. Você foi multado!`)
+} else {
+    console.log(`Você passou no limite permitido.`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
+
+
