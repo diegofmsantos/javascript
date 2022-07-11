84ea2bab3d3b5bd3dccd4301b8e7371eb8b1d869
Diego Santos <105954958+diegofmsantos@users.noreply.github.com>
2022-07-11 14:43:02 -0300

    primeira aula de exercício

---
+++ ex011.js
@@ -0,0 +1,12 @@
+var hora = 00
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora >= 00 && hora < 6) {
+    console.log('Bom madrugada!')
+} else if (hora >= 6 && hora < 12) {
+    console.log('Boa dia!')
+}   else if (hora >= 12 && hora < 18) {
+    console.log('Boa tarde!')
+}   else {
+    console.log('Boa noite!')
+}    
+
