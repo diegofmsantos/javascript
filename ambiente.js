5ef7afee23c9ab7ab57fbbd9aebe2c526642aa41
Diego Santos <105954958+diegofmsantos@users.noreply.github.com>
2022-07-12 12:41:48 -0300

    repetições

--- ambiente.js
+++ ambiente.js
@@ -1,14 +1,15 @@
-var c = 1
-do {
-    console.log(`Passo ${c}`)
-    c++
-} while (c <= 10)
-
-
-
-
 /* var c = 1
 while (c <= 10) {
     console.log('Tudo bem?')
     c++
-} */
+} 
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10) */
+
+for (var c = 1; c <= 5; c++) {
+    console.log(`Passo ${c}`)
+}
