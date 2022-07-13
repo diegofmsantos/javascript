2179bbf90d84415a73e8fe7cfd2862a7ef3e6c32
Diego Santos <105954958+diegofmsantos@users.noreply.github.com>
2022-07-13 15:14:45 -0300

    exc js

--- modelo/script.js
+++ modelo/script.js
@@ -7,13 +7,20 @@ function contar() {
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
     window.alert('[ERRO] Faltam dados!')
     } else {
-            res.innerHTML = 'Contando:'
+            res.innerHTML = 'Contando: <br>'
             var i = Number(ini.value)
             var f = Number(fim.value)
             var p = Number(passo.value)
-
+            if (i < f) {
             for(var c = i; c <= f; c += p) {
                 res.innerHTML += `${c} \u{1F449}` 
             }
+            res.innerHTML += `\u{1F3C1}`
+        } else {
+            for(var c = i;c >= f; c-= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
         }
+        res.innerHTML += `\u{1F3C1}`  
+}       
 }
